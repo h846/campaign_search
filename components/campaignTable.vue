f3bb7e467544863c6eb99d8f99719aa5387ba1a4
Yashiro <Hideki.Yashiro@landsend.co.jp>
2021-05-24 17:19:23 +0900

    table modified

--- components/campaignTable.vue
+++ components/campaignTable.vue
@@ -1,6 +1,19 @@
 <template>
-  <v-card elevation="10" class="pa-10">
-    <v-data-table :headers="headers" :items="rtnList()" :search="search" dense>
+  <v-card elevation="1" class="px-4">
+    <v-data-table
+      :headers="headers"
+      :items="rtnList()"
+      :search="search"
+      :expanded.sync="expanded"
+      item-key="ID"
+      show-expand
+      hide-default-footer
+      dense
+      :page.sync="page"
+      :items-per-page="5"
+      @page-count="pageCount = $event"
+    >
+      <!-- Search Form -->
       <template v-slot:top>
         <v-text-field
           v-model="search"
@@ -9,7 +22,36 @@
           class="mb-2"
         ></v-text-field>
       </template>
+      <!-- Expand -->
+      <template v-slot:expanded-item="{ headers, item }">
+        <td :colspan="headers.length">{{ item }}</td>
+      </template>
+      <!-- Table Body -->
+      <template v-slot:item="{ item, expand, isExpanded }">
+        <tr :class="{ gray: item.isExpired }">
+          <td>
+            <v-btn @click="expand(!isExpanded)" text>詳細</v-btn>
+          </td>
+          <td class="d-block d-sm-table-cell">
+            {{ item["概要"] }}
+          </td>
+          <td class="d-block d-sm-table-cell">
+            {{ item["種別"] }}
+          </td>
+          <td class="d-block d-sm-table-cell">
+            {{ item["コード"] }}
+          </td>
+          <td class="d-block d-sm-table-cell">
+            {{ item["開始日"] }}
+          </td>
+          <td class="d-block d-sm-table-cell">
+            {{ item["終了日"] }}
+          </td>
+        </tr>
+      </template>
+
       <!-- 資料カラムをリンク化-->
+      <!--
       <template v-slot:item.ref="{ item }">
         <div
           v-for="(i, idx) in item.ref"
@@ -22,7 +64,11 @@
           </a>
         </div>
       </template>
+      -->
     </v-data-table>
+    <v-card-actions>
+      <v-pagination v-model="page" :length="20"></v-pagination>
+    </v-card-actions>
   </v-card>
 </template>
 <script>
@@ -30,19 +76,25 @@ export default {
   props: { campaignList: Array },
   data: function() {
     return {
+      page: 1,
+      pageCount: 0,
       search: "",
       headers: [
-        { text: "種別", value: "種別" },
-        { text: "コード", value: "コード" },
-        { text: "資料", value: "ref" },
+        { text: "", value: "data-table-expand" },
+        { text: "概要", value: "概要", sortable: false },
+        { text: "種別", value: "種別", sortable: false },
+        { text: "コード", value: "コード", sortable: false },
         { text: "開始日", value: "開始日" },
-        { text: "終了日", value: "終了日" },
-        { text: "概要", value: "概要" },
-        { text: "取得方法", value: "取得方法" },
-        { text: "送料", value: "特典内容" },
-        { text: "詳細1", value: "使用条件1" },
-        { text: "詳細2", value: "使用条件2" }
-      ]
+        { text: "終了日", value: "終了日" }
+        /*
+        { text: "資料", value: "ref", sortable: false },
+        { text: "取得方法", value: "取得方法", sortable: false },
+        { text: "送料", value: "特典内容", sortable: false },
+        { text: "詳細1", value: "使用条件1", sortable: false },
+        { text: "詳細2", value: "使用条件2", sortable: false },
+        */
+      ],
+      expanded: []
     };
   },
   methods: {
@@ -53,9 +105,9 @@ export default {
     },
     rtnList: function() {
       //リスト整形処理
-      let list = this.campaignList.map(val => {
+      let list = this.campaignList.map((val, idx) => {
         //format date
-        val["終了日"] = this.$moment(val["終了日"]).format("YYYY-MM-DD");
+        val["終了日"] = this.$moment(val["終了日"]).format("YYYY/MM/DD");
         // If the campaign period has already expired, flag it.
         if (this.validDate(val["終了日"])) {
           val["isExpired"] = true;
@@ -69,7 +121,8 @@ export default {
         //空文字除去
         tmp = tmp.filter(val => !!val);
         val.ref = tmp;
-
+        //ID付与
+        val.ID = idx;
         return val;
       });
 
@@ -83,11 +136,6 @@ export default {
       });
       return list;
     }
-  },
-  created() {
-    let list = this.rtnList();
-
-    console.log(list);
   }
 };
 </script>
