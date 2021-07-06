2f90b957d1498840df64e0c52f961ee852fb43ce
Hideki Yashiro <h846@outlook.com>
2021-07-06 10:07:33 +0900

    admin

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,13 +1,15 @@
 <template>
   <v-row no-gutters justify="center" class="mt-8">
-    <v-col cols="6">
-      <v-card max-width="374" class="pa-2" outlined>
+    <v-col align="center">
+      <v-card max-width="400" class="pa-2" outlined>
         <v-card-title>キャンペーン追加</v-card-title>
         <v-form>
           <v-container>
             <v-row>
-              <v-col>
+              <v-col sm="6" cols="12">
                 <v-select
+                  outlined
+                  color="success"
                   v-model="selectTypes"
                   :items="types"
                   item-text="label"
@@ -17,11 +19,72 @@
                   single-line
                   hint="キャンペーンの種別を選択してください"
                   persistent-hint
+                  dense
                 ></v-select>
               </v-col>
+              <v-col sm="6" cols="12">
+                <v-text-field
+                  outlined
+                  v-model="campCode"
+                  color="success"
+                  label="キャンペーンコード"
+                  :rules="rules.code"
+                  required
+                  hint="キャンペーンコードを入力してください"
+                  persistent-hint
+                  dense
+                ></v-text-field>
+              </v-col>
             </v-row>
-            <v-row>
-              <v-col> </v-col>
+            <p class="ma-0 pa-0 my-5">キャンペーン期間を選択してください</p>
+            <v-row justify="center" align="center">
+              <v-col cols="5">
+                <v-menu max-width="290px" min-width="290px" offset-y>
+                  <!-- ポップアップを追加する要素にv-on="on" -->
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      slot="activator"
+                      v-model="fromDate"
+                      label="開始日"
+                      readonly
+                      v-on="on"
+                    />
+                  </template>
+                  <!-- ポップアップされる内容-->
+                  <v-date-picker
+                    v-model="fromDate"
+                    locale="jp-ja"
+                    :day-format="date => new Date(date).getDate()"
+                    no-title
+                  />
+                </v-menu>
+              </v-col>
+              <v-col cols="1" class="pa-0"
+                ><v-icon large color="blue-grey darken-2"
+                  >mdi-arrow-right</v-icon
+                ></v-col
+              >
+              <v-col cols="5">
+                <v-menu max-width="290px" min-width="290px" offset-y>
+                  <!-- ポップアップを追加する要素にv-on="on" -->
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      slot="activator"
+                      v-model="toDate"
+                      label="終了日"
+                      readonly
+                      v-on="on"
+                    />
+                  </template>
+                  <!-- ポップアップされる内容-->
+                  <v-date-picker
+                    v-model="toDate"
+                    locale="jp-ja"
+                    :day-format="date => new Date(date).getDate()"
+                    no-title
+                  />
+                </v-menu>
+              </v-col>
             </v-row>
           </v-container>
         </v-form>
@@ -54,8 +117,16 @@ export default {
         { label: "お祝い", value: "お祝い" },
         { label: "お詫び", value: "お詫び" },
         { label: "リサイクル", value: "リサイクル" }
-      ]
+      ],
+      campCode: "",
+      fromDate: "",
+      toDate: "",
+
+      rules: {
+        code: [val => (val.length > 0 ? val : "入力必須項目です")]
+      }
     };
-  }
+  },
+  computed: {}
 };
 </script>
